5aa8ab708747ca8b87ab74fe1b55e632e4178736
alailsonko <aalailson3@gmail.com>
2024-12-22 19:30:49 -0300

    save changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const heroMessage = document.querySelector('.hero-message');
+    if (!heroMessage) return;
     setTimeout(() => {
         heroMessage.classList.add('show');
     }, 500); // Adjust delay as needed
@@ -43,3 +44,56 @@ document.addEventListener('DOMContentLoaded', function() {
         },
     });
 });
+
+function toggleMobileNav() {
+    console.log('clicked');
+    const hamburger = document.querySelector('.hamburger');
+    const mobileNav = document.getElementById('mobile-nav');
+    const closeBtn = document.querySelector('.close-btn');
+
+    mobileNav.classList.toggle('active');
+
+    // should change display mobile nav to none or flex
+    if (mobileNav.classList.contains('active')) {
+        mobileNav.style.display = 'flex';
+        hamburger.style.display = 'none';
+        closeBtn.style.display = 'block';
+    } else {
+        mobileNav.style.display = 'none';
+        if (window.innerWidth <= 1250) {
+            hamburger.style.display = 'flex';
+            closeBtn.style.display = 'none';
+        }
+    }
+}
+
+// Close mobile nav when clicking outside
+document.addEventListener('click', function(event) {
+    const mobileNav = document.getElementById('mobile-nav');
+    const hamburger = document.querySelector('.hamburger');
+    const isClickInsideNav = mobileNav.contains(event.target);
+    const isClickOnHamburger = hamburger.contains(event.target);
+    const closeBtn = document.querySelector('.close-btn');
+
+    if (!isClickInsideNav && !isClickOnHamburger && mobileNav.classList.contains('active')) {
+        mobileNav.classList.remove('active');
+        hamburger.classList.remove('active');
+        closeBtn.style.display = 'none';
+    }
+});
+
+function updateHamburgerDisplay() {
+    const mobileNav = document.getElementById('mobile-nav');
+    const hamburger = document.querySelector('.hamburger');
+    if (window.innerWidth <= 1250 && !mobileNav.classList.contains('active')) {
+        hamburger.style.display = 'flex';
+    } else {
+        hamburger.style.display = 'none';
+    }
+}
+
+// Initial check
+updateHamburgerDisplay();
+
+// Update on window resize
+window.addEventListener('resize', updateHamburgerDisplay);
